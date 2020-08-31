a7c84176b7390be5b2006829d38ab7f0fb4624f4
Vadigar <45230690+Vadigar@users.noreply.github.com>
2020-09-01 02:14:56 +0300

    Update Longest-graphic-key.cpp

--- Longest-graphic-key.cpp
+++ Longest-graphic-key.cpp
@@ -1,20 +1,31 @@
-﻿// Longest-graphic-key.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+int main(){
+	vector<int> perm(9);
+	generate(perm.begin(), perm.end(), [] {static int i = 8; return i--; });
+	auto max_perm = make_pair(perm, DBL_MIN);
+	for_each(perm.begin(), perm.end(), [](int value) { cout << value << ' '; });
+	cout.put('\n');
+	for (double lenght(0.); prev_permutation(perm.begin(), perm.end()); lenght = 0.) {
+		vector<bool> visited(9, false);
+		adjacent_find(perm.begin(), perm.end(),
+			[&](int first, int second) -> bool {
+				int first_x = first % 3, first_y = first / 3, second_x = second % 3, second_y = second / 3;
+				if (!visited[second])
+					lenght += hypot(first_x - second_x, first_y - second_y);
+				visited[first] = true;
+				if ((first_x % 2 == second_x % 2) && (first_y % 2 == second_y % 2))
+					visited[(first_x + second_x) / 2 + (first_y + second_y) / 2 * 3] = true;
+				return visited[second];
+			});
+		if (lenght > max_perm.second)
+			max_perm = { perm , lenght };
+	}
+	for_each(max_perm.first.begin(), max_perm.first.end(), [](int value) { cout << value << ' '; });
+	cout.put('\n');
+	cout << "max lenght = " << max_perm.second << '\n';
+	return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
